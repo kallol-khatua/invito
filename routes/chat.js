ccebc7cbb8cf05b71d041f656ab5c85853d46c52
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-03-03 02:03:32 +0530

    fixed chat ui issue

--- routes/chat.js
+++ routes/chat.js
@@ -9,51 +9,55 @@ const ExpressError = require("../utils/ExpressError");
 const router = express.Router();
 
 
-router.get("/dashboard", isLoggedIn, async(req, res, next) => {
-    // send user except the xurrent user
-    // res.cookie('user', JSON.stringify(req.user));
-    let users = await User.find({_id: {$nin: [req.user._id]}});
-    res.render("chats/dashboard.ejs", {users});
+router.get("/dashboard", isLoggedIn, async (req, res, next) => {
+    try {
+        // send user except the xurrent user
+        // res.cookie('user', JSON.stringify(req.user));
+        let users = await User.find({ _id: { $nin: [req.user._id] } });
+        res.render("chats/dashboard.ejs", { users });
+    } catch (err) {
+        console.log(err)
+    }
 });
 
-router.post("/saveChat",isLoggedIn, async(req, res, next) => {
-    try{
+router.post("/saveChat", isLoggedIn, async (req, res, next) => {
+    try {
         let newChat = new Chat({
             receiver_id: req.body.receiver_id,
             sender_id: req.body.sender_id,
             message: req.body.message
         });
         await newChat.save();
-        res.status(200).send({success: true, message: "chat saved", data: newChat});
-    }catch(error) {
-        res.status(400).send({success: false, message: error.message});
+        res.status(200).send({ success: true, message: "chat saved", data: newChat });
+    } catch (error) {
+        res.status(400).send({ success: false, message: error.message });
     }
 })
 
 router.post("/searchUser", async (req, res, next) => {
-    try{
+    try {
         console.log(req.body.username);
-        let user = await User.findOne({username: req.body.username});
+        let user = await User.findOne({ username: req.body.username });
         console.log(user);
-        if(!user){
-            return res.status(200).send({success: false, message: error.message});
+        if (!user) {
+            return res.status(200).send({ success: false, message: error.message });
         }
         console.log(user);
-        res.status(200).send({success: true, user: user});
-    }catch(error) {
+        res.status(200).send({ success: true, user: user });
+    } catch (error) {
         console.log(error)
-        res.status(200).send({success: false, message: error.message});
+        res.status(200).send({ success: false, message: error.message });
     }
 })
 
 // render group page with user created groups 
-router.get("/groups", isLoggedIn, async(req, res, next) => {
-    let groups = await Group.find({creator: req.user._id});
-    res.render("chats/groups.ejs", {groups});
+router.get("/groups", isLoggedIn, async (req, res, next) => {
+    let groups = await Group.find({ creator: req.user._id });
+    res.render("chats/groups.ejs", { groups });
 });
 
 // create new group and save details to database
-router.post("/groups", isLoggedIn, async(req, res, next) => {
+router.post("/groups", isLoggedIn, async (req, res, next) => {
     let newGroup = new Group({
         creator: req.user,
         name: req.body.name
@@ -63,12 +67,12 @@ router.post("/groups", isLoggedIn, async(req, res, next) => {
 });
 
 // find member and send data to frontend
-router.post("/members", isLoggedIn, async(req, res, next) => {
-    try{
+router.post("/members", isLoggedIn, async (req, res, next) => {
+    try {
         // group id came form the frontend at the time of ajax request 
         // console.log(req.body); .find({_id: {$nin: [req.user._id]}});
         // console.log(req.body.groupId)
-        
+
         let users = await User.aggregate([
             {
                 $lookup: {
@@ -80,7 +84,7 @@ router.post("/members", isLoggedIn, async(req, res, next) => {
                             $match: {
                                 $expr: {
                                     $and: [
-                                        {$eq: ["$group_id", req.body.groupId]}
+                                        { $eq: ["$group_id", req.body.groupId] }
                                     ]
                                 }
                             }
@@ -99,16 +103,16 @@ router.post("/members", isLoggedIn, async(req, res, next) => {
         ]);
 
         // console.log(users);
-        res.status(200).send({success: true, users});
-    }catch(err) {
-        res.status(400).send({success: false, message: err.message});
-    }   
+        res.status(200).send({ success: true, users });
+    } catch (err) {
+        res.status(400).send({ success: false, message: err.message });
+    }
 });
 
 // add members for a group
-router.post("/addMembers", isLoggedIn, async(req, res, next) => {
-    try{
-        await Member.deleteMany({group_id: req.body.group_id})
+router.post("/addMembers", isLoggedIn, async (req, res, next) => {
+    try {
+        await Member.deleteMany({ group_id: req.body.group_id })
         let data = [];
         for (member of req.body.addMemberId) {
             data.push({
@@ -117,62 +121,62 @@ router.post("/addMembers", isLoggedIn, async(req, res, next) => {
             })
         }
         await Member.insertMany(data);
-        res.status(200).send({success: true, message: "Members updated"});
-    }catch(err) {
-        res.status(400).send({success: false, message: err.message});
-    }   
+        res.status(200).send({ success: true, message: "Members updated" });
+    } catch (err) {
+        res.status(400).send({ success: false, message: err.message });
+    }
 });
 
 // when click on the group share link deciding how can join the group
-router.get("/share-group/:id", isLoggedIn, async(req, res, next) => {
-    try{
-        let {id} = req.params;
+router.get("/share-group/:id", isLoggedIn, async (req, res, next) => {
+    try {
+        let { id } = req.params;
         let group = await Group.findById(id).populate("creator");
 
         // if group not found or group does not exist then show page not found page
-        if(!group){
+        if (!group) {
             return next(new ExpressError(404, "Page not found"));
         }
 
         // creator of group will not be able to join the group
-        if(String(group.creator._id) == String(req.user._id)){
-            return res.render("./chats/shareGroup.ejs", {group: group, message: "Creator"})
-        } 
-        
-        // if already member then will not be able to join the group
-        let member = await Member.find({group_id: id, member_id: req.user._id});
-        // console.log(member);
-        if(member.length > 0) {
-            return res.render("./chats/shareGroup.ejs", {group: group, message: "Member"})
+        if (String(group.creator._id) == String(req.user._id)) {
+            return res.render("./chats/shareGroup.ejs", { group: group, message: "Creator" })
         }
-        
-        return res.render("./chats/shareGroup.ejs", {group: group, message: "join"})
-    }catch(err){
+
+        // if already member then will not be able to join the group
+        let member = await Member.find({ group_id: id, member_id: req.user._id });
+        // console.log(member);
+        if (member.length > 0) {
+            return res.render("./chats/shareGroup.ejs", { group: group, message: "Member" })
+        }
+
+        return res.render("./chats/shareGroup.ejs", { group: group, message: "join" })
+    } catch (err) {
         // console.log("catch")
         return next(new ExpressError(404, "Page not found"));
     }
 });
 
 // add member from share join link
-router.post("/join-group", async(req, res, next) => {
-    try{
-        let member = await Member.find({group_id: req.body.groupId, member_id: req.user._id});
-        if(member.length > 0) {
-            return res.status(200).send({success: false, message: "already member"});
+router.post("/join-group", async (req, res, next) => {
+    try {
+        let member = await Member.find({ group_id: req.body.groupId, member_id: req.user._id });
+        if (member.length > 0) {
+            return res.status(200).send({ success: false, message: "already member" });
         }
-        let newMember = new Member({group_id: req.body.groupId, member_id: req.user._id});
+        let newMember = new Member({ group_id: req.body.groupId, member_id: req.user._id });
         await newMember.save();
-        return res.status(200).send({success: true, message: "become a member"});
-    }catch(err) {
-        return res.status(200).send({success: false, message: "error"});
+        return res.status(200).send({ success: true, message: "become a member" });
+    } catch (err) {
+        return res.status(200).send({ success: false, message: "error" });
     }
 })
 
 // render group dashboard
-router.get("/group-dashboard", isLoggedIn, async(req, res, next) => {
-    let createdGroup = await Group.find({creator: req.user._id});
-    let joinedGroup = await Member.find({member_id: req.user._id}).populate("group_id");
-    res.render("chats/group-dashboard.ejs", {createdGroup: createdGroup, joinedGroup: joinedGroup});
+router.get("/group-dashboard", isLoggedIn, async (req, res, next) => {
+    let createdGroup = await Group.find({ creator: req.user._id });
+    let joinedGroup = await Member.find({ member_id: req.user._id }).populate("group_id");
+    res.render("chats/group-dashboard.ejs", { createdGroup: createdGroup, joinedGroup: joinedGroup });
 });
 
 module.exports = router;
